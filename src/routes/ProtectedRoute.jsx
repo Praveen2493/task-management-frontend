c7a364a2e95b84008fce2a1c3d2459b579c50e94
Praveen2493 <rajpraveen2412@gmail.com>
2026-06-19 20:17:14 +0530

    Add Netlify redirects

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({children}) => {
 
-    const isAuthenticated = true;
+    const isAuthenticated = false;
   
     return isAuthenticated 
     ? children : <Navigate to="/login"/>;
